734bc4183ce2bf025d95335897e040255491fd04
Nikhil1017 <115337705+Nikhil1017@users.noreply.github.com>
2023-05-01 09:04:54 +0530

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,21 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      width: {
+        '128':'648px',
+        '164':'180px',
+      },
+      height: {
+        '128':'704px',
+        '164':'33.6px',
       },
     },
   },
