702ce0c72d09c6603ed48be7acc636839131d6eb
Nikhil1017 <115337705+Nikhil1017@users.noreply.github.com>
2023-05-06 18:20:24 +0530

    more responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,15 @@ export default function Home() {
   const [inputValue, setInputValue] = useState('');
   const [checked, setChecked] = useState(false);
   return (
-    <div
-    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <div className=" flex  flex-col w-128 h-128 justify-center items-center">
+      <div className=" flex  flex-col w-128 h-128 justify-center items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
              <Image
               src="/logo.svg"
               alt="Lookscout Logo"
               width={180}
               height={33.6}
+              className="flex"
             />
-            <div className="flexjustify-center  items-center w-128 h-27">
+            <div className="flex first-line:justify-center  items-center w-128 h-27">
             <div  className="flex bg-shadow w-128 h-27 rounded-t-md" >
               <div >
               <Image
@@ -22,84 +21,84 @@ export default function Home() {
                alt="account"
                width={32}
                height={32}
-               className="pt-6 ml-12"
+               className="flex pt-6 ml-12"
                />
                <Image
                src="/acctxt.svg"
                alt="accounttxt"
                width={57}
                height={20}
-               className="pt-1 ml-35.5px "
+               className="pt-1 flex ml-35.5px "
                />
                </div >   
                <Image src="/line.svg" alt="line" width={133}
-               height={0} className="pb-6" />          
+               height={0} className=" flex pb-6" />          
                <div >
                <Image
                src="/personal.svg"
                alt="personal"
                width={32}
                height={32}
-               className="pt-6"
+               className="flex pt-6"
                />
                <Image
                src="/personaltxt.svg"
                alt="accounttxt"
                width={59}
                height={20}
-               className="pt-1 mr-8"
+               className="flex pt-1 mr-8"
                />
                </div >
                <Image src="/line.svg" alt="line" width={133}
-               height={0}  className="pb-6"/>
+               height={0}  className="flex pb-6"/>
                <div >
                 <Image
                src="/billing.svg"
                alt="billing"
                width={32}
                height={32}
-               className="pt-6"
+               className="flex pt-6"
                />
                <Image
                src="/billingtxt.svg"
                alt="accounttxt"
                width={41}
                height={20}
-               className="pt-1"
+               className="flex pt-1"
                />
                </div >
                <Image src="/line.svg" alt="line" width={133}
-               height={0}  className="pb-6"/>
+               height={0}  className="flex pb-6"/>
                <div >
                 <Image
                src="/done.svg"
                alt="done"
                width={32}
                height={32}
-               className="pt-7 mr-12"
+               className="flex pt-7 mr-12"
                />
                <Image
                src="/donetxt.svg"
                alt="accounttxt"
                width={36}
                height={20}
-               className="pt-1"
+               className="flex pt-1"
                />
                </div >
 
                </div >
                </div>
-              <div className="flex flex-col w-128 h-128 p-x12">
+              <div className="flex flex flex-col w-128 h-128 p-x12">
             <Image
               src="/nametxt.svg"
               alt="name"
               width={38}
               height={12}
-              className="mt-8 ml-12"
+              className="flex mt-8 ml-12"
             />
     <form>
       <label>
-        <input type="text" className="border rounded-md mt-2 w-552px mx-12 h-46px border-boxshadow " value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <input type="text" className="flex border rounded-md mt-2 w-552px mx-12 h-46px border-boxshadow " value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
       </label>
     </form>
     <Image
@@ -107,11 +106,11 @@ export default function Home() {
               alt="name"
               width={41}
               height={12}
-              className="mt-1.5 ml-12"
+              className="flex mt-1.5 ml-12"
             />
     <form>
       <label>
-        <input type="text" className="border rounded-md mt-2 w-552px mx-12 h-46px border-boxshadow " value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <input type="text" className="flex border rounded-md mt-2 w-552px mx-12 h-46px border-boxshadow " value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
         
             
               </label>
@@ -121,7 +120,7 @@ export default function Home() {
               alt="name"
               width={214}
               height={14}
-              className="ml-12 -mt-2.5"
+              className="flex ml-12 mt-2.5"
             />  
     <div className="flex flex-row">
       <div>
@@ -130,7 +129,7 @@ export default function Home() {
               alt="name"
               width={71}
               height={12}
-              className="mt-6 ml-12"
+              className="flex mt-6 ml-12"
               
             />
             <form>
@@ -144,7 +143,7 @@ export default function Home() {
               alt="name"
               width={178.66}
               height={12}
-              className="ml-12 -mt-2.5"
+              className="flex ml-12 mt-2.5"
               
             />
     <label className="ml-12">
@@ -168,7 +167,7 @@ export default function Home() {
             />
         <form>
         <label>
-        <input type="text" className="border rounded-md mt-2 w-268px h-46px -ml-0.5 border-boxshadow " value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <input type="text" className="flex border rounded-md mt-2 w-268px h-46px -ml-0.5 border-boxshadow " value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
 
             
        </label>
@@ -178,7 +177,7 @@ export default function Home() {
               alt="name"
               width={168}
               height={12}
-              className="ml-1 -mt-2.5"
+              className="ml-1 mt-2.5"
               
             />       
        </div>
@@ -194,7 +193,6 @@ export default function Home() {
             /> 
               </button>
        </div>
-       </div >
        </div >
   )
 }
